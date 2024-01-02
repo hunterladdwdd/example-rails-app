059b8d9f2e34e18f45bc2d62d228dc15748c806c
hunter.ladd <dhunter.ladd@gmail.com>
2024-01-02 11:28:45 -0800

    Remove flash usage, puts statement, and add before_action filter to set @item ivar on specific actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :update, :show, :destroy]
+
   def new
     @item = Item.new
   end
@@ -9,47 +11,35 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to items_path
     else
-      flash[:danger] = @item.errors.full_messages
-
-      redirect_to new_item_path
+      render :new
     end
   end
 
-  def edit
-    @item = Item.find(params['id'])
-  end
+  def edit; end
 
   def update
-    @item = Item.find(params['id'])
-
     if @item.update(item_params)
       redirect_to items_path
     else
-      flash[:danger] = @item.errors.full_messages
-
-      redirect_to item_path(@item)
+      render :edit
     end
   end
 
-  def show
-    @item = Item.find(params['id'])
-  end
+  def show; end
 
   def index
     @items = Item.all.order(id: :desc)
   end
 
   def destroy
-    @item = Item.find(params['id'])
-
-    puts @item.inspect
-
-    @item.delete if @item
-
     redirect_to items_path
   end
 
   private
+
+  def set_item
+    @item = Item.find(params['id'])
+  end
 
   def item_params
     params.require(:item).permit(:name, :description)
